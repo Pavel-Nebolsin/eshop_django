c1596d20a8bc43ca465f9be1ae723537d9675dff
Pavel-Nebolsin <49613408+Pavel-Nebolsin@users.noreply.github.com>
2023-07-13 10:21:17 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -9,7 +9,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$oy9ixg5syk$rlaqlks$@x^&e%p=t-cp^ddf=9!b!t7kd2^+'
+SECRET_KEY = 'mysecretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
